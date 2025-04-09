b139e17c32e41f62ade0b19ea61c7cba159e9823
DirkTunderman <dirk.tunderman@outlook.com>
2025-04-09 08:11:41 +0200

    less chars

--- app/src/transcript_pipeline/processor/chunked_processor.py
+++ app/src/transcript_pipeline/processor/chunked_processor.py
@@ -339,7 +339,7 @@ class ChunkedProcessor:
         logger.info(f"MAXIMUM ACCEPTABLE LENGTH: {max_acceptable_length} characters")
 
         # Check if transcript needs trimming
-        charater_amount_goal = config["ai"]["length"] * 820
+        charater_amount_goal = config["ai"]["length"] * 750
         if processed_transcript_length > charater_amount_goal:
             logger.warning(f"Transcript is too long by {processed_transcript_length - charater_amount_goal} characters")
 
